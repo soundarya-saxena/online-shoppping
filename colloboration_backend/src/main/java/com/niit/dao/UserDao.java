d8b897f7bc12d2b87cb146d3f4382931138e3196
venkatdhayalan <vengatvenkie@gmail.com>
2018-01-24 21:38:54 +0530

    Registration completed

---
+++ colloboration_backend/src/main/java/com/niit/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.niit.dao;
+
+import com.niit.model.User;
+
+public interface UserDao {
+
+	void registration(User user);
+	boolean isEmailValid(String email);
+	User login(User user);
+}
